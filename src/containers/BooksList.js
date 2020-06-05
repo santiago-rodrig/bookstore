aeaeaaf8d11070a33a7263fd26632e10209f9a72
Santiago Rodriguez <santo1996.29@gmail.com>
2020-06-05 19:02:49 -0400

    feature: CategoryFilter component, filtering!

--- src/containers/BooksList.js
+++ src/containers/BooksList.js
@@ -3,10 +3,11 @@ import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import Book from '../components/Book';
 import { removeBook } from '../actions';
+import CategoryFilter from './CategoryFilter';
 
 const mapStateToProps = state => {
-  const { books } = state;
-  return { books };
+  const { books, filter } = state;
+  return { books, filter };
 };
 
 const booksCollection = books => {
@@ -23,27 +24,38 @@ const mapDispatchToProps = dispatch => ({
   },
 });
 
-const Component = ({ books, removeBook }) => (
-  <table>
-    <thead>
-      <tr>
-        <th>ID</th>
-        <th>Category</th>
-        <th>Title</th>
-        <th>Actions</th>
-      </tr>
-    </thead>
-    <tbody>
-      {booksCollection(books).map(
-        book => <Book key={book.id} book={book} removeBook={removeBook} />,
-      )}
-    </tbody>
-  </table>
-);
+const Component = ({ books, filter, removeBook }) => {
+  const filteredBooks = () => {
+    const booksInArray = booksCollection(books);
+    if (filter === 'all') return booksInArray;
+    return booksInArray.filter(book => book.category === filter);
+  };
+  return (
+    <>
+      <CategoryFilter />
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Category</th>
+            <th>Title</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {filteredBooks().map(
+            book => <Book key={book.id} book={book} removeBook={removeBook} />,
+          )}
+        </tbody>
+      </table>
+    </>
+  );
+};
 
 Component.propTypes = {
   books: PropTypes.objectOf(PropTypes.object).isRequired,
   removeBook: PropTypes.func.isRequired,
+  filter: PropTypes.string.isRequired,
 };
 
 const BooksList = connect(mapStateToProps, mapDispatchToProps)(Component);
---
+++ src/containers/CategoryFilter.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { changeFilter } from '../actions';
+
+const mapDispatchToProps = dispatch => ({
+  changeFilter: filter => {
+    dispatch(changeFilter(filter));
+  },
+});
+const categories = [
+  'action',
+  'biography',
+  'history',
+  'horror',
+  'kids',
+  'learning',
+  'sci-fi',
+].map(category => (
+  <option key={category} value={category}>{category}</option>
+));
+const Component = ({ changeFilter }) => {
+  const handleChange = e => {
+    changeFilter(e.target.value);
+  };
+  return (
+    <form id="filter-form">
+      <fieldset>
+        <legend>
+          Filter details
+        </legend>
+        <div className="field">
+          <label htmlFor="filter">
+            Choose a filter
+            <select
+              id="filter"
+              name="filter"
+              defaultValue="all"
+              required
+              onChange={handleChange}
+            >
+              <option value="all">all</option>
+              {categories}
+            </select>
+          </label>
+        </div>
+      </fieldset>
+    </form>
+  );
+};
+Component.propTypes = {
+  changeFilter: PropTypes.func.isRequired,
+};
+const CategoryFilter = connect(null, mapDispatchToProps)(Component);
+export default CategoryFilter;
