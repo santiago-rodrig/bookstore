8749cde6ef63b8d8826894bee5023647d0fad422
Santiago Rodriguez <santo1996.29@gmail.com>
2020-06-08 16:13:38 -0400

    feature: nav component

---
+++ src/components/Nav.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import NavLink from './NavLink';
+
+const Component = () => (
+  <nav>
+    <NavLink className="nav-link" text="books" />
+    <NavLink className="nav-link" text="categories" />
+  </nav>
+);
+
+export default Component;
